feb3c98cb3600dfc1366eedb986cfc3c4bd74778
Lance <lance@johto.com>
2020-07-04 18:51:09 -0400

    fixed for windows and added qol stuff

--- src/main.py
+++ src/main.py
@@ -1,20 +1,36 @@
 from bs4 import BeautifulSoup
 from urllib.parse import unquote_plus
+from os import path, mkdir
+import platform
 import requests
 import sys
-import os
+
 # Script to download a precise set of OST from khinsider.com
 # arg1 = name of the game found on khinsider.com exemple: pokemon-platinum
+DOWNLOAD_DIR = path.join(".","downloads")
 KHINSIDER_BASE_URL = "https://downloads.khinsider.com"
 KHINSIDER_OST_SECTION = "game-soundtracks/album"
 UNIQUE_SONGS = {}
 
 def has_href_and_mp3(tag):
     is_valid = False
-    if tag.get('href', None) and '.mp3' in tag['href'] and tag['href'] not in UNIQUE_SONGS:
+    if tag.name == "a" and\
+            tag.get('href', None) and\
+            '.mp3' in tag['href']:
         is_valid = True
+        handle_song_link(tag)
     return is_valid
 
+def handle_song_link(link):
+    raw_url = link['href']
+    if "http" not in raw_url:
+        raw_url = "{}{}".format(KHINSIDER_BASE_URL, link['href'])
+    clean_url = double_unquote(raw_url)
+    song_name = clean_url.split('/')[-1]
+    UNIQUE_SONGS[song_name] = raw_url
+
+def double_unquote(url):
+    return unquote_plus(unquote_plus(url))
 
 def find_unique_songs(url):
     global UNIQUE_SONGS
@@ -23,10 +39,6 @@ def find_unique_songs(url):
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, features="html5lib")
         links = soup.find_all(has_href_and_mp3)
-        for link in links:
-            clean_url = unquote_plus(unquote_plus("{}{}".format(KHINSIDER_BASE_URL, link['href'])))
-            song_name = clean_url.split('/')[-1]
-            UNIQUE_SONGS[song_name] = "{}{}".format(KHINSIDER_BASE_URL, link['href'])
 
 def find_download_link():
     global UNIQUE_SONGS
@@ -37,28 +49,43 @@ def find_download_link():
         if r.status_code == 200:
             soup = BeautifulSoup(r.text, features="html5lib")
             download_links = soup.find_all(has_href_and_mp3)
-            for download_link in download_links:
-                UNIQUE_SONGS[song] = download_link['href']
-                print("Download link: {}\n".format(download_link['href']))
 
-def download_songs():
+def download_songs(album_dir):
     print("Downloading found unique songs ...")
     for song in UNIQUE_SONGS.keys():
         download_r = requests.get(UNIQUE_SONGS[song], allow_redirects=True)
         if download_r.status_code == 200:
             print("Downloading : {}".format(song))
-            open('downloads/{}'.format(song), 'wb').write(download_r.content)
+            print("Using URL : {}".format(UNIQUE_SONGS[song]))
+            open(path.join(album_dir, song), 'wb').write(download_r.content)
         else:
-            print("There seems to have a problem with the download link ...")
+            print("There seems to have a problem with the download link, status_code was: {}"\
+                .format(download_r.status_code))
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
+        print("=-" * 10)
+        print("KH1NS1D3R SCR4P3R")
+        print("=-" * 10)
         album = sys.argv[1]
+        album_dir = path.join(DOWNLOAD_DIR, album)
+        if not path.exists(DOWNLOAD_DIR):
+            print("Creating Download directory {} ...".format(DOWNLOAD_DIR))
+            mkdir(DOWNLOAD_DIR)
+        if not path.exists(album_dir):
+            print("Creating Album Download directory {} ...".format(album_dir))
+            mkdir(album_dir)
+
         url = "{}/{}/{}".format(KHINSIDER_BASE_URL, KHINSIDER_OST_SECTION, album)
-        print("URL : {}".format(url))
+        print("=" * 20)
         find_unique_songs(url)
+
+        print("=" * 20)
         find_download_link()
-        download_songs()
+
+        print("=" * 20)
+        download_songs(album_dir)
+        print("\nFinished ...")
     else:
         print("Incorrect number of arguments.")
 
